d44cce487fc4585fda8f5fd88e26c5148f74e044
John Amakye <john.amakye@minneapolismn.gov>
2016-12-12 14:57:29 -0600

    ajax post working

--- public/scripts/cc3.js
+++ public/scripts/cc3.js
@@ -5,23 +5,27 @@ $( document ).ready( function(){
   //start addJokeButton on click
   $( '#addJokeButton' ).on( 'click', function(){
     console.log( 'addJokeButton on click');
+    postData();
+    // getData();
   }); // end addJokeButton on click
 
-  // postData function
+  // start postData
   var postData = function(){
-    console.log( 'in postData' );
+    console.log( 'in postData?' );
+
     // assemble object to send
-    var newJoke = [
+    var newJoke =
       {
         whoseJoke: $("#whoseJokeIn").val(),
         jokeQuestion: $("#questionIn").val(),
         punchLine: $("#punchlineIn").val()
-      }];
+      };
    // end object to send
+   console.log("in newJoke:", newJoke);
 
     $.ajax({
       type: 'POST',
-      url: '/sendJoke',
+      url: '/postJoke',
       data: newJoke,
       success: function( response ){
         console.log( 'back from post call:', response );
@@ -31,11 +35,23 @@ $( document ).ready( function(){
         console.log( 'error with ajax call...');
       }
     });
-  }; // end postData function
+  }; // end postData
 
-
-
-
+//   // start getJoke
+//   var getJoke = function(){
+//     console.log( 'in getJoke' );
+//     $.ajax({
+//       type: 'GET',
+//       url: '/getJoke',
+//       success: function( response ){
+//         console.log( 'back from get call:', response);
+// // need object from server side
+//     },
+//       error: function(){
+//         console.log( 'error with ajax call...');
+//       }
+//     });
+//   }; // end getJoke
 
 
 
--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var bodyParser = require( 'body-parser' );
 var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
 // use bodyParser.urlencoded throughout the app with this:
 app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( express.static( 'public' ) );
+
 // initial jokes provided by the client
 jokes = [
   {
@@ -41,5 +43,31 @@ app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'views/index.html' ) );
 }); // end base url
 
+// testPost
+app.post( '/postJoke', urlEncodedParser, function( req, res ){
+  console.log( 'postJoke url hit req.body:', req.body );
+jokes.push(req.body);
+console.log("This is in jokes:", jokes);
+  // do work here
+  // assemble object to return
+  var objectToReturn =
+  {
+    field0: 'I came from postJoke on server'
+  }; // end object to return
+  // return objectToReturn
+  res.send( objectToReturn );
+}); // end testPost
 
-app.use( express.static( 'public' ) );
+
+// testGet
+app.get( '/getJoke', function( req, res ){
+  console.log( 'getJoke url hit' );
+  // do work here
+  // assemble object to return
+  var objectToReturn =
+  {
+    field0: 'I came from getJoke on server'
+  }; // end object to return
+  // return objectToReturn
+  res.send( jokes );
+}); // end testGet
